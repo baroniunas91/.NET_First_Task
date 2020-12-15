60f2c303c4b884b224362372728b5ccd7e435851
Edgaras <baroniunas@gmail.com>
2020-12-15 17:10:14 +0200

    Add calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            Console.WriteLine("To close application write - stop");
+            string combination = Console.ReadLine();
+            string operation = "";
+
+            while (combination != "stop")
+            {
+                for (int i = 0; i < combination.Length; i++)
+                {
+                    if (combination[i] == '+')
+                    {
+                        operation = "+";
+                    }
+                    else if (combination[i] == '*')
+                    {
+                        operation = "*";
+                    }
+                    else if (combination[i] == '-')
+                    {
+                        operation = "-";
+                    }
+                    else if (combination[i] == '/')
+                    {
+                        operation = "/";
+                    }
+                }
+
+                string[] numbers = combination.Split(operation);
+                List<int> numbersList = new List<int>();
+                foreach (var item in numbers)
+                {
+                    if (item != "")
+                    {
+                        int num = int.Parse(item.ToString());
+                        numbersList.Add(num);
+                    }
+
+                }
+                double result = 0;
+                for (int i = 0; i < numbersList.Count; i++)
+                {
+                    if (operation == "+")
+                    {
+                        if (i == 0)
+                        {
+                            result = Convert.ToDouble(numbersList[i]);
+
+                        }
+                        else
+                        {
+                            result += Convert.ToDouble(numbersList[i]);
+                        }
+                    }
+                    else if (operation == "-")
+                    {
+                        if (i == 0)
+                        {
+                            result = Convert.ToDouble(numbersList[i]);
+                        }
+                        else
+                        {
+                            result -= Convert.ToDouble(numbersList[i]);
+                        }
+                    }
+                    else if (operation == "*")
+                    {
+                        if (i == 0)
+                        {
+                            result = Convert.ToDouble(numbersList[i]);
+                        }
+                        else
+                        {
+                            result *= Convert.ToDouble(numbersList[i]);
+                        }
+                    }
+                    else if (operation == "/")
+                    {
+                        if (i == 0)
+                        {
+                            result = Convert.ToDouble(numbersList[i]);
+                        }
+                        else
+                        {
+                            result /= Convert.ToDouble(numbersList[i]);
+                        }
+                    }
+                }
+                Console.WriteLine($"Result: {result}");
+                combination = Console.ReadLine();
+            }
+        }
+    }
+}
